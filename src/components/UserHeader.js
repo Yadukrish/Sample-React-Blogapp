9c96b46400b7409801590ab6f8bb3d60e3f575ac
Yadukrish <40061489+Yadukrish@users.noreply.github.com>
2021-05-16 20:36:29 +0530

    Sample React Blog App

---
+++ src/components/UserHeader.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { connect } from "react-redux";
+
+class UserHeader extends React.Component {
+  render() {
+    const user = this.props.user;
+    if (!user) return null;
+
+    return <div className="header">{user.name}</div>;
+  }
+}
+const mapStateToProps = (state, ownProps) => {
+  return {
+    user: state.users.find((user) => user.id === ownProps.userIdy),
+  };
+};
+export default connect(mapStateToProps)(UserHeader);
